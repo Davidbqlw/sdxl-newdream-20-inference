a89c4d31940f10c4446a264ac60f52b4fcbd2880
Davidbqlw <110333954+Davidbqlw@users.noreply.github.com>
2024-10-25 08:49:26 +0700

    Update pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -1340,6 +1340,6 @@ def infer(request: TextToImageRequest, pipeline: StableDiffusionXLPipeline) -> I
         width=request.width,
         height=request.height,
         generator=generator,
-        end_cfg=0.7,
+        end_cfg=0.55,
         num_inference_steps=20,
     ).images[0]
